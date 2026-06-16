1a3b76f2556d5d4c19c336cfb5cd2bc4fb2dda6f
Sanap Nisha <nishasanap2005@gmail.com>
2026-06-16 21:55:52 +0530

    Add files via upload

---
+++ backend/controllers/userController.js
@@ -0,0 +1,17 @@
+const User = require('../models/User');
+
+// @desc    Get current logged in user
+// @route   GET /api/user/profile
+// @access  Private
+exports.getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user.id);
+
+    res.status(200).json({
+      success: true,
+      data: user,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
